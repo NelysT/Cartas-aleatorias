06cb6b7c2a397a4b04767a13815e0bb6681b97c4
NelysT <139475672+NelysT@users.noreply.github.com>
2023-08-23 10:30:34 +0000

    actualizando cambios en js pero no salen los suits aleatoriamente a la par, ha perdido la configuracion del html luego de ejecutar el js, supongo q sea una custion de importar y tampoco se me esta cumpliendo el for para q de color al heart y diamond

--- src/app.js
+++ src/app.js
@@ -7,5 +7,27 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+  let numeros = document.getElementById("numeros");
+  let imgTop = document.getElementById("imgTop");
+  let imgButton = document.getElementById("imgButton");
+
+  let numbers = ["A", "2", "3", "4", "5", "6", "7", "8", "9", "J", "Q", "K"];
+  let suits = ["♦", "♥", "♠", "♣"];
+
+  for (let i = 0; i < suits.lenght; i++) {
+    if (suits[i] === "♥" || suits[i] === "♦") {
+      suits[i].style.color = red;
+      return suits[i];
+    } else {
+      return suits[i];
+    }
+  }
+
+  let generateRandom = array => {
+    let random = Math.floor(Math.random() * suits.length);
+    return array[random];
+  };
+  numeros.innerHTML = generateRandom(numbers);
+  imgTop.innerHTML = generateRandom(suits);
+  imgButton.innerHTML = generateRandom(suits);
 };
