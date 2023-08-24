310492e5abff14d776bd35be4914b881251f3dcb
NelysT <139475672+NelysT@users.noreply.github.com>
2023-08-24 18:00:45 +0000

    guardando cambios finales

--- src/app.js
+++ src/app.js
@@ -8,24 +8,15 @@ import "./assets/img/4geeks.ico";
 window.onload = function generarCarta() {
   //write your code here
   let numeros = document.querySelector("p");
-  let imgTop = document.querySelector("#imgTop"); //no se si hay q definir una variable para cada uno de los palos
+  let imgTop = document.querySelector("#imgTop");
   let imgButton = document.querySelector("#imgButton");
 
   let numbers = ["A", "2", "3", "4", "5", "6", "7", "8", "9", "J", "Q", "K"];
   let suits = ["♦", "♥", "♠", "♣"];
 
-  for (let i = 0; i < suits.lenght; i++) {
-    if (suits[i] === "♥" || suits[i] === "♦") {
-      suits[i].style.color = "red"; //tampoco se si se le da el color asi
-      console.log(suits[i]);
-      return suits[i]; //entiendo q no estoy retornando esto a ningun sitio
-    } else {
-      return suits[i];
-    }
-  }
-
   let generateRandom = array => {
     let random = Math.floor(Math.random() * suits.length);
+
     if (array[random] === "♥" || array[random] === "♦") {
       imgTop.classList.add("text-danger");
       imgButton.classList.add("text-danger");
@@ -37,6 +28,7 @@ window.onload = function generarCarta() {
     }
     return array[random];
   };
+
   let generarNumeros = generateRandom(numbers);
   let generarSuits = generateRandom(suits);
 
@@ -47,10 +39,4 @@ window.onload = function generarCarta() {
   //agregando el boton
   let btn = document.getElementById("btn");
   btn.addEventListener("click", generarCarta);
-  //agregar boton para el timer
-  let btnTimer = document.getElementById("btnTimer");
-  btnTimer.addEventListener("click", setTimeout); //como hago para q se ejecute solo cuando le den click y cuando
-se de   setTimeout(function() {
-    generarCarta();
-  }, 10000);
 };
